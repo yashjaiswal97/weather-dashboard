33f2d15e616d262b69895f1fbd35438b60e9d386
Yash Jaiswal <yashj2897@gmail.com>
2025-05-01 19:26:11 +0530

    api update

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -25,3 +25,20 @@ export const fetchForecastByCity = async (city: string) => {
   });
   return res.data;
 };
+
+export const searchCities = async (query: string) => {
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const res = await fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`
+    );
+    return res.json();
+  };
+
+  export const fetchAQIByCoords = async (lat: number, lon: number) => {
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    );
+    return res.json();
+  };
+  
